4f4efa815a2271d2bf6c5dbf079d0b6c36903a9c
s4bb4k <ch.morales@outlook.com>
2020-10-05 15:23:56 -0500

    fin sección 4- Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,19 @@
-import { PagesComponent } from './pages/pages.component';
-import { Error404Component } from './pages/error404/error404.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { LoginComponent } from './auth/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// modules
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { Error404Component } from './error404/error404.component';
+
 const routes: Routes = [
-  { 
-    path: '', 
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    ]
-  },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+
+  // path: '/dashboard' PagesRouting
+  // path: '/auth' AuthRouting
+  // path: '/medicos' MedicosRouting
+  // path: '/compras' ComprasRouting
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: Error404Component }
 ];
 
@@ -29,7 +22,11 @@ const routes: Routes = [
   imports: [ 
     RouterModule.forRoot( routes )
   ],
-  exports: [RouterModule]
+  exports: [ 
+    RouterModule, 
+    PagesRoutingModule,
+    AuthRoutingModule  
+  ]
 })
 
 export class AppRoutingModule { }
